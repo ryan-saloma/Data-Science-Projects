03764c62e802b2962535048fc3706a1d220b7cfb
ryan-saloma <ryan.saloma@gmail.com>
2024-07-02 14:10:22 -0400

    Renamed folders, added job posting data set

---
+++ Data Generation/job-posting-db.py
@@ -0,0 +1,35 @@
+
+import requests
+import pandas as pd
+import json
+
+search_terms = ['data', 'analyst', 'District-of-Columbia']
+
+# date_posted = 3days
+# employment_type = FULLTIME
+# query = data analyst in District-of-Columbia
+
+base_url = "https://jsearch.p.rapidapi.com/search" 
+query = "?query=" + "%".join(search_terms) + "&employment_type=FULLTIME&date_posted=week&num_pages=1&page_num="
+page_num = 1
+
+payload = {}
+headers = {
+  'x-rapidapi-host': 'jsearch.p.rapidapi.com',
+  'x-rapidapi-key': 'XXXXX'
+}
+
+url = base_url + query
+df = pd.DataFrame()
+for i in range(50):
+    tmp_url = url + str(i)
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = json.loads(response.text)
+    tmp_df = pd.DataFrame(data["data"])
+    if df.empty: 
+        df = tmp_df.copy()
+    else:
+        df = pd.concat([df,tmp_df], ignore_index=True)
+        df.reset_index()
+
+df.to_csv('job_postings_2024-07-02.csv')
